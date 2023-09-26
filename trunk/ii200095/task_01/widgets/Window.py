2392301d9e6c8ff4443c0cf9a84db395ce7b03c7
#FSociety <80279133+akayohonda@users.noreply.github.com>
2023-09-26 17:30:43 +0300

    decide_issues

--- trunk/ii200095/task_01/widgets/Window.py
+++ trunk/ii200095/task_01/widgets/Window.py
@@ -1,16 +1,6 @@
-from PyQt5.QtWidgets import (
-    QWidget,
-    QVBoxLayout,
-    QHBoxLayout,
-    QPushButton,
-    QRadioButton,
-    QSlider,
-    QFileDialog,
-    QLabel,
-    QProgressBar
-)
-from PyQt5.QtCore import Qt, QVariantAnimation
-from PyQt5.QtGui import QPixmap, QTransform
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QRadioButton, QSlider, QFileDialog, QLabel, QProgressBar
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QTransform
 from widgets.ImagesWidget import ImagesWidget
 from helpers.ImageHelper import ImageHelper
 from PIL import Image
@@ -81,7 +71,7 @@ class Window(QWidget):
         self._layout.addWidget(self._radio_widget, 1)
         self._label1 = QLabel()
         self._label1.setStyleSheet('color: black;')
-        self._label1.setText(f'Kernel size: 0')
+        self._label1.setText('Kernel size: 0')
         self._layout.addWidget(self._label1, 1)
         self._layout.addWidget(self._slider, 1)
         self._layout.addWidget(self._buttons_widget, 1)
@@ -119,11 +109,12 @@ class Window(QWidget):
             kernel_size = self._slider.value()
 
             if self._selected_kernel_option == 'both':
-                self._image_helper.median_filter(image, kernel_width=kernel_size, kernel_height=kernel_size, progress_handler=lambda x: self._progress_bar.setValue(x))
+                self._image_helper.median_filter(image, kernel_width=kernel_size, kernel_height=kernel_size, 
+                progress_handler=self._progress_bar.setValue(x))
             elif self._selected_kernel_option == 'vertical':
-                self._image_helper.median_filter(image, kernel_height=kernel_size, progress_handler=lambda x: print(x))
+                self._image_helper.median_filter(image, kernel_height=kernel_size, progress_handler=print(x))
             else:
-                self._image_helper.median_filter(image, kernel_width=kernel_size, progress_handler=lambda x: print(x))
+                self._image_helper.median_filter(image, kernel_width=kernel_size, progress_handler=print(x))
 
             self._images_widget.set_unnoised_image()
 
