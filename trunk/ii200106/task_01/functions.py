bc10211f4edf033041e39ee746bd711dcc7dce4d
slavenia <slavakovalets@gmail.com>
2023-10-21 22:31:00 +0300

    task_01_fix

--- trunk/ii200106/task_01/functions.py
+++ trunk/ii200106/task_01/functions.py
@@ -1,63 +1,51 @@
 import cv2
-import numpy as np  # для работы с математикой
-import matplotlib.pyplot as plt  # для вывода картинки
-import functions
 import random
 
-def show_image(image, window_name): # показать картинку
+def show_img(image, window_name): # показать картинку
     cv2.imshow(window_name, image)
 
-def noise(image, level, progressBar): # шум
-    buff = 0.1
-    # проходимся по каждому пикселю (x,y) и если рандомом выпадет единица то зарисовываем оттенком серого
-    for x in range(len(image)):
-        progress = x*100/len(image)
-        if int(buff) != int(progress):
-            print(str(int(progress))+"%")
-            progressBar.setValue(int(progress))
-        buff = progress
-        for y in range(len(image[0])):
-            if(random.randrange(0,(102-level)) == 1): # если выпала единица (а шанс меняем с помощью level, который берется со слайдера)
-                                                      # чем больше рейндж, тем меньше шанс единицы
-                random_v = random.randrange(1,5) #чем больше здесь выпало тем ярче пиксель
-                image[x][y] = [random_v*50,random_v*50,random_v*50]
+def give_noises(image, level):
+    tmp = 0.1
+    for x, row in enumerate(image):
+
+        prog = x*100/len(image)
+        if int(tmp) != int(prog):
+            print(str(int(prog))+"%")
+        tmp = prog
+        for y, _ in enumerate(row):
+            if (random.randint(0,(102-level)) == 1):
+                random_v = random.randrange(1, 5)
+                image[x][y] = [random_v*50, random_v*50, random_v*50]
     print('100%')
-    progressBar.setValue(0)
     return image
 
-def denoise(image, threshold, windowX, windowY, progressBar):
-    # шум
-    # (картинка, порог, размер окна Х, размер окна У)
-   
-    # идём по каждому пикселю в (х, у)
-    buff = 0.1
-    for x in range(len(image)):
-        progress = x*100/len(image)
+def distract_noises(image, t, wX, wY):
+    tmp = 0.1
+    for x, row in enumerate(image):
+        prog = x*100/len(image)
 
-        if int(buff) != int(progress):
-            print(str(int(progress)) + "%" )
-            progressBar.setValue(int(progress))
-        buff = progress
+        if int(tmp) != int(prog):
+            print(str(int(prog)) + "%")
+        tmp = prog
 
-        for y in range(len(image[0])):
-            average_pixel = [0,0,0]
-            pixels_in_average = 0
-            # в это время для каждого пикселя смотрим соседей на расстоянии (размер окна \ 2)
-            for x_ in range(int(-1*windowX/2),int(windowX/2) + 1):
-                for y_ in range(int(-1*windowY/2),int(windowY/2) + 1):
-                    if((x+x_>=0) and (y+y_>=0) and (x+x_<len(image)) and (y+y_<len(image[0])) ):
-                        pixels_in_average+=1
-                        average_pixel[0] += image[x+x_][y+y_][0]
-                        average_pixel[1] += image[x+x_][y+y_][1]
-                        average_pixel[2] += image[x+x_][y+y_][2]
-            # для всех соседей включая сам пиксель находим среднюю яркость
-            average_brightness = (average_pixel[0] + average_pixel[1] + average_pixel[2]) / (pixels_in_average*3)
-            average_pixel = [average_pixel[0]/(pixels_in_average), average_pixel[1]/(pixels_in_average), average_pixel[2]/(pixels_in_average)]
-            pixel_brightness = (int(image[x][y][0]) + int(image[x][y][1]) + int(image[x][y][2])) / 3.0
-            # если пиксель ярче средней яркости соседей, то
-            if (abs(pixel_brightness-average_brightness)>threshold):
-                # то пиксель приравниваем к средней яркости
-                image[x][y] = [int(average_pixel[0]),int(average_pixel[1]),int(average_pixel[2])]
-    print ('100%')
-    progressBar.setValue(0)
+        for y, _ in enumerate(row):
+            avg_pixel, pixels_in_avg = check(image, x, y, wX, wY)
+            avg_bright = (avg_pixel[0] + avg_pixel[1] + avg_pixel[2]) / (pixels_in_avg*3)
+            avg_pixel = [avg_pixel[0]/(pixels_in_avg), avg_pixel[1]/(pixels_in_avg), avg_pixel[2]/(pixels_in_avg)]
+            pixel_bright = (int(image[x][y][0]) + int(image[x][y][1]) + int(image[x][y][2])) / 3.0
+            if (abs(pixel_bright-avg_bright)>t):
+                image[x][y] = [int(avg_pixel[0]), int(avg_pixel[1]), int(avg_pixel[2])]
+    print('100%')
     return image
+
+def check(image, x, y, wX, wY):
+    avg_pixel = [0,0,0]
+    pixels_in_avg = 0
+    for x_ in range(int(-1*wX/2),int(wX/2) + 1):
+        for y_ in range(int(-1*wY/2),int(wY/2) + 1):
+            if (x+x_>=0) and (y+y_>=0) and (x+x_<len(image)) and (y+y_<len(image[0])):
+                pixels_in_avg+=1
+                avg_pixel[0] += image[x+x_][y+y_][0]
+                avg_pixel[1] += image[x+x_][y+y_][1]
+                avg_pixel[2] += image[x+x_][y+y_][2]
+    return avg_pixel, pixels_in_avg
--- trunk/ii200106/task_01/main.py
+++ trunk/ii200106/task_01/main.py
@@ -1,87 +1,71 @@
-import cv2 # работа с картинками
-import functions # файл с функциями
+import cv2
+import functions
 
-from PyQt5 import uic, QtTest # интерфейс
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-
-import random 
-import sys 
+from PyQt5 import uic
+from PyQt5.QtWidgets import (
+    QLabel, QSlider, QSpinBox, QPushButton, QDialog, QFileDialog, QApplication
+)
+import sys
 
 
-global IMAGE_GLOBAL # наше изображение
-IMAGE_GLOBAL = cv2.imread("/image.jpg") #первоначальная загрузка изображения
-
-
-class MainWindow(QDialog): # класс окна, тут описан его функционал
-    def __init__(self):    # инициализация
+class MainWindow(QDialog):
+    def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi("ui/MainWindow.ui", self)
         self.setWindowTitle("Пороговый филтр, лаба 1")
         self.show()
-        #----------------- # поиск элементов и присвоение функционала
-        self.textNoise = self.findChild(QLabel, "noise_text")
-        self.textFilter = self.findChild(QLabel, "threshold_text")
 
-        self.sliderNoise = self.findChild(QSlider, "noise_slider")
-        self.sliderFilter = self.findChild(QSlider, "filter_slider")
+        self.image = cv2.imread("image/image.jpg")
+        self.txtNoise = self.findChild(QLabel, "noise_text")
+        self.txtFilter = self.findChild(QLabel, "threshold_text")
+
+        self.slNoise = self.findChild(QSlider, "noise_slider")
+        self.slFilter = self.findChild(QSlider, "filter_slider")
         self.spinBox1 = self.findChild(QSpinBox, "spinbox1")
         self.spinBox2 = self.findChild(QSpinBox, "spinbox2")
 
-        self.buttonLoad = self.findChild(QPushButton, "load_button")
-        self.buttonNoise = self.findChild(QPushButton, "noise_button")
-        self.buttonFilter = self.findChild(QPushButton, "filter_button")
-        self.buttonImage = self.findChild(QPushButton, "show_button")
-
-        self.progressBar = self.findChild(QProgressBar, "progressBar")
+        self.btnLoad = self.findChild(QPushButton, "load_button")
+        self.btnNoise = self.findChild(QPushButton, "noise_button")
+        self.btnFilter = self.findChild(QPushButton, "filter_button")
+        self.btnImage = self.findChild(QPushButton, "show_button")
         #-----------------========
-        self.buttonLoad.clicked.connect(self.loadImage)
-        self.buttonNoise.clicked.connect(self.applyNoise)
-        self.buttonFilter.clicked.connect(self.filterImage)
-        self.buttonImage.clicked.connect(self.showImage)
-        self.sliderNoise.valueChanged.connect(self.sliderNoiseTick)
-        self.sliderFilter.valueChanged.connect(self.sliderFilterTick)
+        self.btnLoad.clicked.connect(self.loadImg)
+        self.btnNoise.clicked.connect(self.approveNoise)
+        self.btnFilter.clicked.connect(self.filterImg)
+        self.btnImage.clicked.connect(self.showImg)
+        self.slNoise.valueChanged.connect(self.noisesTick)
+        self.slFilter.valueChanged.connect(self.filterTick)
         #-----------------========
 
-    def sliderNoiseTick(self): # действие при измененении значений слайдеров
-        self.textNoise.setText("шум: "+str(self.sliderNoise.value()) + "%")
-    def sliderFilterTick(self):
-        self.textFilter.setText("порог: "+str(self.sliderFilter.value()))
+    def noisesTick(self):
+        self.txtNoise.setText("шум: "+str(self.slNoise.value()) + "%")
 
-    def loadImage(self): # загрузить картинку
-        global IMAGE_GLOBAL
+    def filterTick(self):
+        self.txtFilter.setText("порог: "+str(self.slFilter.value()))
+
+    def loadImg(self):
         path = QFileDialog.getOpenFileName()
-        IMAGE_GLOBAL = cv2.imread(path[0])
-        if(True): # если надо выводить по отдельности, то делаем своё окно
-            functions.show_image(IMAGE_GLOBAL, "loaded")
-        else:   # если нет то рисуем в общее
-            functions.show_image(IMAGE_GLOBAL, "main window")
+        self.image = cv2.imread(path[0])
+
+        self.showImg()
 
 
-    def applyNoise(self): # применение шума
-        global IMAGE_GLOBAL
-        IMAGE_GLOBAL = functions.noise(IMAGE_GLOBAL, self.sliderNoise.value(), self.progressBar)
-        if(False): 
-            functions.show_image(IMAGE_GLOBAL, "noise applied")
+    def approveNoise(self):
+        self.image = functions.give_noises(self.image, self.slNoise.value())
+        self.showImg()
+
+    def filterImg(self):
+        self.image = functions.distract_noises(self.image, self.slFilter.value(), self.spinBox1.value(), self.spinBox2.value())
+        self.showImg()
+
+    def showImg(self, wndout = False): # показать картинку
+        if (wndout):
+            functions.show_img(self.image, "results")
         else:
-            functions.show_image(IMAGE_GLOBAL, "main window")
+            functions.show_img(self.image, "main window")
 
-    def filterImage(self): # фильтр
-        global IMAGE_GLOBAL
-        IMAGE_GLOBAL = functions.denoise(IMAGE_GLOBAL, self.sliderFilter.value(), self.spinBox1.value(), self.spinBox2.value(), self.progressBar)
-        if(False): 
-            functions.show_image(IMAGE_GLOBAL, "filtered")
-        else:
-            functions.show_image(IMAGE_GLOBAL, "main window")
 
-    def showImage(self): # показать картинку
-        if(False): 
-            functions.show_image(IMAGE_GLOBAL, "results")
-        else:
-            functions.show_image(IMAGE_GLOBAL, "main window")
-
-# так надо
-app = QApplication(sys.argv)
-launcher = MainWindow()
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    launcher = MainWindow()
+    app.exec_()
