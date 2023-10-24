12539d340dbd5818ee5733e456442b9d7a2e29ba
Nikita <tacetprophetam@mail.ru>
2023-10-25 00:11:31 +0300

    task01_fix4

--- trunk/ii200113/task_01/helpers/ImageHelper.py
+++ trunk/ii200113/task_01/helpers/ImageHelper.py
@@ -6,36 +6,30 @@ class ImageHelper:
         with Image.open(path) as image:
             image_to_noise_buff = image.copy()
             image_pixels_counter = image_to_noise_buff.load()
-
             count = 0
             while (count < broken_pixels_count):
                 x_coord = random.randint(0, abs(image.size[0] - 1))
                 y_coord = random.randint(0, abs(image.size[1] - 1))
-
                 image_pixels_counter[x_coord, y_coord] = (0, 0, 0)
                 count += 1
-
             image_to_noise_buff.save(path)
 
 def get_pixel_colors(image, i, j):
     red_colors = []
     green_colors = [] 
     blue_colors = []
-
     for k_i in range(kernel_width):
         if k_i + i + 1 < image_width:
             r, g, b = image.getpixel((k_i + i + 1, j))
             red_colors.append(r)
             green_colors.append(g) 
             blue_colors.append(b)
-
     for k_j in range(kernel_height):
         if k_j + j + 1 < image_height:
             r, g, b = image.getpixel((i, k_j + j + 1))
             red_colors.append(r)
             green_colors.append(g)
             blue_colors.append(b)
-            
     return red_colors, green_colors, blue_colors
 
 
@@ -47,19 +41,15 @@ def get_median_color(color_list):
 def median_filter(image, progress_handler, kernel_width=1, kernel_height=1):
     image_copy = image.copy()
     width, height = image_copy.size
-    
     for i in range(width):
         for j in range(height):
             colors = get_pixel_colors(image_copy, i, j)  
             r = get_median_color(colors[0])
             g = get_median_color(colors[1])
             b = get_median_color(colors[2])
-            
             image_copy.putpixel((i, j), (r, g, b))
-
             count += 1
             progress_handler(count)
-
     image_copy.save('unnoised_image.png')
         image_to_unnoise_buff = image.copy()
         image_width_buff, image_height_buff = image_to_unnoise_buff.size
@@ -70,35 +60,27 @@ def median_filter(image, progress_handler, kernel_width=1, kernel_height=1):
                 red_colors_buff = []
                 green_colors_buff = []
                 blue_colors_buff = []
-
                 for k_i in range(kernel_width):
                     if k_i + i + 1 < image_width_buff:
                         red, green, blue = image_to_unnoise_buff.getpixel((k_i + i + 1, j))
                         red_colors_buff.append(red)
                         green_colors_buff.append(green)
                         blue_colors_buff.append(blue)
-
                 for k_j in range(kernel_height):
                     if k_j + j + 1 < image_height_buff:
                         red, green, blue = image_to_unnoise_buff.getpixel((i, k_j + j + 1))
                         red_colors_buff.append(red)
                         green_colors_buff.append(green)
                         blue_colors_buff.append(blue)
-
                 red_colors_buff.sort()
                 green_colors_buff.sort()
                 blue_colors_buff.sort()
-
                 image_pixels_counter[i, j] = (
                     red_colors_buff[len(red_colors_buff) // 2],
                     green_colors_buff[len(green_colors_buff) // 2],
                     blue_colors_buff[len(blue_colors_buff) // 2]
                 )
-
-
             count += 1
             progress_handler(int(((count * 100 / (image_width_buff * image_height_buff * 3)) - 0.014) * 1000))
-
         print(count)
-
         image_to_unnoise_buff.save('images/unnoised_image.png')
--- trunk/ii200113/task_01/widgets/Window.py
+++ trunk/ii200113/task_01/widgets/Window.py
@@ -7,7 +7,6 @@ from PIL import Image
 class AppWindow(QWidget):
     def __init__(self):
         super().__init__()
-
         self.image_helper = ImageHelper()
         self._setup_ui()
 
@@ -19,77 +18,53 @@ class AppWindow(QWidget):
             '''
         )
         self.setGeometry(50, 50, 1300, 700)
-
         self._main_layout = QVBoxLayout()
-
         self._buttons_widget = QWidget()
         self._buttons_layout = QHBoxLayout()
-
         self._open_file_btn = self._create_button('Open file', self._open_file_clicked)
         self._buttons_layout.addWidget(self._open_file_btn, 1)
-        
         self._remove_noise_btn = self._create_button('Remove noise', self._remove_noise_clicked)
         self._buttons_layout.addWidget(self._remove_noise_btn, 1)
-        
         self._add_noise_btn = self._create_button('Create noise button', self._add_noise_clicked)
         self._buttons_layout.addWidget(self._add_noise_btn, 1)
-
         self._buttons_widget.setLayout(self._buttons_layout)
-
         self._images_widget = ImagesWidget()
-
         self._radio_widget = QWidget()
         self._radio_layout = QHBoxLayout()
-
         self._horizontal_radio = self._create_radio('Horizontal axis', 'horizontal')
         self._radio_layout.addWidget(self._horizontal_radio, 1, Qt.AlignCenter)
-        
         self._vertical_radio = self._create_radio('Vertical axis', 'vertical')
         self._radio_layout.addWidget(self._vertical_radio, 1, Qt.AlignCenter)
-        
         self._both_radio = self._create_radio('Both', 'both', True)
         self._radio_layout.addWidget(self._both_radio, 1, Qt.AlignCenter)
-        
         self._radio_widget.setLayout(self._radio_layout)
-
         self._kernel_slider = QSlider(Qt.Horizontal)
         self._kernel_slider.setRange(1, 20)
         self._kernel_slider.valueChanged.connect(self._on_kernel_change)
-        self._kernel_slider.setStyleSheet(''' 
-            background-color: gray;
-        ''')
-        
+        self._kernel_slider.setStyleSheet('''background-color: gray;''')
         self._progress_bar = QProgressBar(self, minimum=0, maximum=100, textVisible=False)
         self._progress_bar.setValue(20)
-        self._progress_bar.setStyleSheet('''
-            background-color: black;
-            height: 100px;
-        ''')
-        
+        self._progress_bar.setStyleSheet('''background-color: black;
+        height: 100px;''')
         self._main_layout.addWidget(self._images_widget, 6)
         self._main_layout.addWidget(self._radio_widget, 1)
-        
         self._kernel_label = QLabel()
         self._kernel_label.setText('Kernel size: 0')
         self._main_layout.addWidget(self._kernel_label, 1)
-        
         self._main_layout.addWidget(self._kernel_slider, 1)
         self._main_layout.addWidget(self._buttons_widget, 1)
         self._main_layout.addWidget(self._progress_bar, 1)
-
         self.setLayout(self._main_layout)
 
     def _create_button(self, text, signal):
         button = QPushButton(text)
         button.clicked.connect(signal)
-        button.setStyleSheet('''
-            height: 50px;
+        button.setStyleSheet('''height: 50px;
             background-color: black;
             outline: none;
             font-size: 24px;
             color: white;
-            border-radius: 12px;
-        ''')
+            border-radius: 12px;''')
         return button
     
     def _create_radio(self, text, axis, checked=False):
@@ -111,10 +86,8 @@ class AppWindow(QWidget):
     def _remove_noise_clicked(self):
         image = Image.open(self._file_path)
         kernel_size = self._kernel_slider.value()
-        
         if self._selected_axis == 'both':
-            self.image_helper.median_filter(image, kernel_width=kernel_size, 
-                kernel_height=kernel_size, progress_handler=self._update_progress)
+            self.image_helper.median_filter(image, kernel_width=kernel_size, kernel_height=kernel_size, progress_handler=self._update_progress)
         elif self._selected_axis == 'vertical':
             self.image_helper.median_filter(image, kernel_height=kernel_size)
         else:
