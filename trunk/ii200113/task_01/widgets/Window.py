84948354f32b8928b2d964be2e287c797da3e9b9
Nikita <tacetprophetam@mail.ru>
2023-10-24 23:59:01 +0300

    task01_fix3

--- trunk/ii200113/task_01/widgets/Window.py
+++ trunk/ii200113/task_01/widgets/Window.py
@@ -4,11 +4,11 @@ from widgets.ImagesWidget import ImagesWidget
 from helpers.ImageHelper import ImageHelper
 from PIL import Image
 
-class Window(QWidget):
+class AppWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.ihi = ImageHelper()
+        self.image_helper = ImageHelper()
         self._setup_ui()
 
     def _setup_ui(self):
@@ -20,112 +20,114 @@ class Window(QWidget):
         )
         self.setGeometry(50, 50, 1300, 700)
 
-        self._layout_inicializator = QVBoxLayout()
+        self._main_layout = QVBoxLayout()
 
-        self._widget_setter = QWidget()
-        self._buttons_layout_inicializator = QHBoxLayout()
+        self._buttons_widget = QWidget()
+        self._buttons_layout = QHBoxLayout()
 
-        self._open_file_button = self._create_button('Open file', self._open_file_button_tapped)
-        self._buttons_layout_inicializator.addWidget(self._open_file_button, 1)
-        self._remove_noise_button = self._create_button('Remove noise', self._remove_noise_button_tapped)
-        self._buttons_layout_inicializator.addWidget(self._remove_noise_button, 1)
-        self._button_creator = self._create_button('Create noise button', self._button_creator_tapped)
-        self._buttons_layout_inicializator.addWidget(self._button_creator, 1)
+        self._open_file_btn = self._create_button('Open file', self._open_file_clicked)
+        self._buttons_layout.addWidget(self._open_file_btn, 1)
+        
+        self._remove_noise_btn = self._create_button('Remove noise', self._remove_noise_clicked)
+        self._buttons_layout.addWidget(self._remove_noise_btn, 1)
+        
+        self._add_noise_btn = self._create_button('Create noise button', self._add_noise_clicked)
+        self._buttons_layout.addWidget(self._add_noise_btn, 1)
 
-        self._widget_setter.setLayout(self._buttons_layout_inicializator)
+        self._buttons_widget.setLayout(self._buttons_layout)
 
         self._images_widget = ImagesWidget()
 
-        self._radio_widget_inicializator = QWidget()
-        self._radio_widget_init = QHBoxLayout()
+        self._radio_widget = QWidget()
+        self._radio_layout = QHBoxLayout()
 
-        self._h_kernel_button_inicializator = self._create_radio_button('Horizontal axis', 'horizontal')
-        self._v_kernel_button_inicializator = self._create_radio_button('Vertical axis', 'vertical')
-        self._both_axises_button = self._create_radio_button('Both', 'both', is_checked=True)
+        self._horizontal_radio = self._create_radio('Horizontal axis', 'horizontal')
+        self._radio_layout.addWidget(self._horizontal_radio, 1, Qt.AlignCenter)
+        
+        self._vertical_radio = self._create_radio('Vertical axis', 'vertical')
+        self._radio_layout.addWidget(self._vertical_radio, 1, Qt.AlignCenter)
+        
+        self._both_radio = self._create_radio('Both', 'both', True)
+        self._radio_layout.addWidget(self._both_radio, 1, Qt.AlignCenter)
+        
+        self._radio_widget.setLayout(self._radio_layout)
 
-        self._radio_widget_init.addWidget(self._h_kernel_button_inicializator, 1, alignment=Qt.AlignmentFlag.AlignCenter)
-        self._radio_widget_init.addWidget(self._v_kernel_button_inicializator, 1, alignment=Qt.AlignmentFlag.AlignCenter)
-        self._radio_widget_init.addWidget(self._both_axises_button, 1, alignment=Qt.AlignmentFlag.AlignCenter)
-        self._radio_widget_inicializator.setLayout(self._radio_widget_init)
+        self._kernel_slider = QSlider(Qt.Horizontal)
+        self._kernel_slider.setRange(1, 20)
+        self._kernel_slider.valueChanged.connect(self._on_kernel_change)
+        self._kernel_slider.setStyleSheet(''' 
+            background-color: gray;
+        ''')
+        
+        self._progress_bar = QProgressBar(self, minimum=0, maximum=100, textVisible=False)
+        self._progress_bar.setValue(20)
+        self._progress_bar.setStyleSheet('''
+            background-color: black;
+            height: 100px;
+        ''')
+        
+        self._main_layout.addWidget(self._images_widget, 6)
+        self._main_layout.addWidget(self._radio_widget, 1)
+        
+        self._kernel_label = QLabel()
+        self._kernel_label.setText('Kernel size: 0')
+        self._main_layout.addWidget(self._kernel_label, 1)
+        
+        self._main_layout.addWidget(self._kernel_slider, 1)
+        self._main_layout.addWidget(self._buttons_widget, 1)
+        self._main_layout.addWidget(self._progress_bar, 1)
 
-        self._slider_inicializator = QSlider(Qt.Orientation.Horizontal)
-        self._slider_inicializator.setRange(1, 20)
-        self._slider_inicializator.valueChanged.connect(self._slider_inicializator_value_changed)
-        self._slider_inicializator.setStyleSheet(
-            '''
-                background-color: gray;
-            '''
-        )
+        self.setLayout(self._main_layout)
 
-        self._progress_barinicializator = QProgressBar(self, minimum=0, maximum=100, textVisible=False,objectName="GreenProgressBar")
-        self._progress_barinicializator.setValue(20)
-        self._progress_barinicializator.setStyleSheet(
-            '''
-                background-color: black;
-                height: 100px;
-            '''
-        )
-
-        self._layout_inicializator.addWidget(self._images_widget, 6)
-        self._layout_inicializator.addWidget(self._radio_widget_inicializator, 1)
-        self._label1_inicializator = QLabel()
-        self._label1_inicializator.setStyleSheet('color: black;')
-        self._label1_inicializator.setText('Kernel size: 0')
-        self._layout_inicializator.addWidget(self._label1_inicializator, 1)
-        self._layout_inicializator.addWidget(self._slider_inicializator, 1)
-        self._layout_inicializator.addWidget(self._widget_setter, 1)
-        self._layout_inicializator.addWidget(self._progress_barinicializator, 1)
-
-        self.setLayout(self._layout_inicializator)
-
-    def _create_button(self, title, signal):
-        button = QPushButton(title)
+    def _create_button(self, text, signal):
+        button = QPushButton(text)
         button.clicked.connect(signal)
-        button.setStyleSheet(
-            '''
-                height: 50px;
-                background-color: black;
-                outline: none;
-                font-size: 24px;
-                color: white;
-                border-radius: 12px;
-            '''
-        )
-
+        button.setStyleSheet('''
+            height: 50px;
+            background-color: black;
+            outline: none;
+            font-size: 24px;
+            color: white;
+            border-radius: 12px;
+        ''')
         return button
+    
+    def _create_radio(self, text, axis, checked=False):
+        radio = QRadioButton(text)
+        radio.axis = axis
+        radio.toggled.connect(self._on_radio_toggle)
+        radio.setChecked(checked)
+        radio.setStyleSheet('color: black;')
+        return radio
 
-    def _create_radio_button(self, title, axis, is_checked=False):
-        radio_button = QRadioButton(title)
-        radio_button.axis = axis
-        radio_button.toggled.connect(self._radio_button_tapped)
-        radio_button.setChecked(is_checked)
-        radio_button.setStyleSheet('color: black;')
+    def _on_radio_toggle(self):
+        self._selected_axis = self.sender().axis
 
-        return radio_button
+    def _open_file_clicked(self):
+        path = QFileDialog.getOpenFileName(self, 'Open file', '', 'Image files (*.png)')[0]
+        self._images_widget.set_noised_image(path)
+        self._file_path = path
 
-    def _remove_noise_button_tapped(self):
-        with Image.open(self._path) as image:
-            k_size = self._slider_inicializator.value()
+    def _remove_noise_clicked(self):
+        image = Image.open(self._file_path)
+        kernel_size = self._kernel_slider.value()
+        
+        if self._selected_axis == 'both':
+            self.image_helper.median_filter(image, kernel_width=kernel_size, 
+                kernel_height=kernel_size, progress_handler=self._update_progress)
+        elif self._selected_axis == 'vertical':
+            self.image_helper.median_filter(image, kernel_height=kernel_size)
+        else:
+            self.image_helper.median_filter(image, kernel_width=kernel_size)
+            
+        self._images_widget.set_unnoised_image()
 
-            if self._selected_kernel_option == 'both':
-                self.ihi.median_filter(image, kernel_width=k_size, kernel_height=k_size, progress_handler=self._progress_barinicializator.setValue())
-            elif self._selected_kernel_option == 'vertical':
-                self.ihi.median_filter(image, kernel_height=k_size)
-            else:
-                self.ihi.median_filter(image, kernel_width=k_size)
+    def _add_noise_clicked(self):
+        self.image_helper.noise_image(self._file_path, 900)
+        self._images_widget.set_noised_image(self._file_path)
 
-            self._images_widget.set_unnoised_image()
+    def _on_kernel_change(self):
+        self._kernel_label.setText(f'Kernel size: {self._kernel_slider.value()}')
 
-    def _button_creator_tapped(self):
-        self.ihi.noise_image(self._path, 900)
-        self._images_widget.set_noised_image(self._path)
-
-    def _radio_button_tapped(self):
-        self._selected_kernel_option = self.sender().axis
-
-    def _open_file_button_tapped(self):
-        self._path = QFileDialog.getOpenFileName(self, "Open file", "", "All files (*.png)")[0]
-        self._images_widget.set_noised_image(self._path)
-
-    def _slider_inicializator_value_changed(self):
-        self._label1_inicializator.setText(f'Kernel size: {self._slider_inicializator.value()}')
+    def _update_progress(self, value):
+        self._progress_bar.setValue(value)
