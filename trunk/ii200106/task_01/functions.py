57d4c72d25946ae9b9895008d9515ff9bc2c16cd
slavenia <slavakovalets@gmail.com>
2023-10-24 14:59:58 +0300

    task_01_another_fix

--- trunk/ii200106/task_01/functions.py
+++ trunk/ii200106/task_01/functions.py
@@ -20,7 +20,7 @@ def give_noises(image, level):
     print('100%')
     return image
 
-def distract_noises(image, t, width_X, width_Y):
+def distract_noises(image, t, x_width, y_width):
     tmp = 0.1
     for x, row in enumerate(image):
         prog = x*100/len(image)
@@ -30,7 +30,7 @@ def distract_noises(image, t, width_X, width_Y):
         tmp = prog
 
         for y, _ in enumerate(row):
-            avg_pixel, pixels_in_avg = check(image, x, y, width_X, width_Y)
+            avg_pixel, pixels_in_avg = check(image, x, y, x_width, y_width)
             avg_bright = (avg_pixel[0] + avg_pixel[1] + avg_pixel[2]) / (pixels_in_avg*3)
             avg_pixel = [avg_pixel[0]/(pixels_in_avg), avg_pixel[1]/(pixels_in_avg), avg_pixel[2]/(pixels_in_avg)]
             pixel_bright = (int(image[x][y][0]) + int(image[x][y][1]) + int(image[x][y][2])) / 3.0
@@ -39,11 +39,11 @@ def distract_noises(image, t, width_X, width_Y):
     print('100%')
     return image
 
-def check(image, x, y, width_X, width_Y):
+def check(image, x, y, x_width, y_width):
     avg_pixel = [0,0,0]
     pixels_in_avg = 0
-    for x_ in range(int(-1*width_X/2),int(width_X/2) + 1):
-        for y_ in range(int(-1*width_Y/2),int(width_Y/2) + 1):
+    for x_ in range(int(-1*x_width/2),int(x_width/2) + 1):
+        for y_ in range(int(-1*y_width/2),int(y_width/2) + 1):
             if (x+x_>=0) and (y+y_>=0) and (x+x_<len(image)) and (y+y_<len(image[0])):
                 pixels_in_avg+=1
                 avg_pixel[0] += image[x+x_][y+y_][0]
