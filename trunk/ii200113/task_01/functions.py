bfbc7073981fc86995f1fcb6e98389660420bb6b
Nikita <tacetprophetam@mail.ru>
2023-11-10 22:54:05 +0300

    task01_fix17

--- trunk/ii200113/task_01/functions.py
+++ trunk/ii200113/task_01/functions.py
@@ -1,51 +1,43 @@
 import cv2
 import random
 
-def pokaz_photo(kartinka, window_name): # показать картинку
+def pokaz_photo(kartinka, window_name): 
     cv2.imshow(window_name, kartinka)
 
 def shum(kartinka, level): # шум
     buffer = 0.1
-    # проходимся по каждому пикселю (x,y) и если рандомом выпадет единица то зарисовываем оттенком серого
-    for x, row in enumerate(kartinka):
+    for x, stroka in enumerate(kartinka):
 
         pokazatel = x*100/len(kartinka)
         if int(buffer) != int(pokazatel):
             print(str(int(pokazatel))+"%")
         buffer = pokazatel
-        for y, _ in enumerate(row):
-            # если выпала единица (а шанс меняем с помощью level, который берется со слайдера)
-            if (random.randint(0,(102-level)) == 1):
-                # чем больше рейндж, тем меньше шанс единицы
-                random_v = random.randrange(1,5) #чем больше здесь выпало тем ярче пиксель
-                kartinka[x][y] = [random_v*50,random_v*50,random_v*50]
+        for y, _ in enumerate(stroka):
+            zarandomlennoe_v = random.randrange(1,5) 
+            kartinka[x][y] = [zarandomlennoe_v*50,zarandomlennoe_v*50,zarandomlennoe_v*50]
     print('100%')
     return kartinka
 
 def deshum(kartinka, threshold, oknox, oknoy):
     '''
-    шум
-    (картинка, порог, размер окна Х, размер окна У)
-    идём по каждому пикселю в (х, у)
+    shum
+    (image, porog, window size Х, window size У)
+    go throw the every pixel (х, у)
     '''
     buffer = 0.1
-    for x, row in enumerate(kartinka):
+    for x, stroka in enumerate(kartinka):
         pokazatel = x*100/len(kartinka)
 
         if int(buffer) != int(pokazatel):
             print(str(int(pokazatel)) + "%" )
         buffer = pokazatel
 
-        for y, _ in enumerate(row):
-            # в это время для каждого пикселя смотрим соседей на расстоянии (размер окна \ 2)
+        for y, _ in enumerate(stroka):
             sredniy_pixel, pixels_in_average = check_pixels(kartinka, x, y, oknox, oknoy)
-            # для всех соседей включая сам пиксель находим среднюю яркость
             sredniy_yarkost = (sredniy_pixel[0] + sredniy_pixel[1] + sredniy_pixel[2]) / (pixels_in_average*3)
             sredniy_pixel = [sredniy_pixel[0]/(pixels_in_average), sredniy_pixel[1]/(pixels_in_average), sredniy_pixel[2]/(pixels_in_average)]
             pixel_brightness = (int(kartinka[x][y][0]) + int(kartinka[x][y][1]) + int(kartinka[x][y][2])) / 3.0
-            # если пиксель ярче средней яркости соседей, то
             if (abs(pixel_brightness-sredniy_yarkost)>threshold):
-                # то пиксель приравниваем к средней яркости
                 kartinka[x][y] = [int(sredniy_pixel[0]),int(sredniy_pixel[1]),int(sredniy_pixel[2])]
     print ('100%')
     return kartinka
