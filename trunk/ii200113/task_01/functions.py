342c604054d3ff2477522c128968b90c49f1aa12
Nikita <tacetprophetam@mail.ru>
2023-11-10 23:18:18 +0300

    task_01_aaaa

---
+++ trunk/ii200113/task_01/functions.py
@@ -0,0 +1,55 @@
+import cv2
+import random
+
+def pokaz_photo(kartinka, window_name): 
+    cv2.imshow(window_name, kartinka)
+
+def shum(kartinka): 
+    buffer = 0.1
+    for x, stroka in enumerate(kartinka):
+
+        pokazatel = x*100/len(kartinka)
+        if int(buffer) != int(pokazatel):
+            print(str(int(pokazatel))+"%")
+        buffer = pokazatel
+        for y, _ in enumerate(stroka):
+            zarandomlennoe_v = random.randrange(1,5) 
+            kartinka[x][y] = [zarandomlennoe_v*50,zarandomlennoe_v*50,zarandomlennoe_v*50]
+    print('100%')
+    return kartinka
+
+def deshum(kartinka, threshold, oknox, oknoy):
+    '''
+    shum
+    (image, porog, window size Х, window size У)
+    go throw the every pixel (х, у)
+    '''
+    buffer = 0.1
+    for x, stroka in enumerate(kartinka):
+        pokazatel = x*100/len(kartinka)
+
+        if int(buffer) != int(pokazatel):
+            print(str(int(pokazatel)) + "%" )
+        buffer = pokazatel
+
+        for y, _ in enumerate(stroka):
+            sredniy_pixel, pixels_in_average = check_pixels(kartinka, x, y, oknox, oknoy)
+            sredniy_yarkost = (sredniy_pixel[0] + sredniy_pixel[1] + sredniy_pixel[2]) / (pixels_in_average*3)
+            sredniy_pixel = [sredniy_pixel[0]/(pixels_in_average), sredniy_pixel[1]/(pixels_in_average), sredniy_pixel[2]/(pixels_in_average)]
+            pixel_brightness = (int(kartinka[x][y][0]) + int(kartinka[x][y][1]) + int(kartinka[x][y][2])) / 3.0
+            if (abs(pixel_brightness-sredniy_yarkost)>threshold):
+                kartinka[x][y] = [int(sredniy_pixel[0]),int(sredniy_pixel[1]),int(sredniy_pixel[2])]
+    print ('100%')
+    return kartinka
+
+def check_pixels(kartinka, x, y, oknox, oknoy):
+    sredniy_pixel = [0,0,0]
+    pixels_in_average = 0
+    for x_ in range(int(-1*oknox/2),int(oknox/2) + 1):
+        for y_ in range(int(-1*oknoy/2),int(oknoy/2) + 1):
+            if (x+x_>=0) and (y+y_>=0) and (x+x_<len(kartinka)) and (y+y_<len(kartinka[0])):
+                pixels_in_average+=1
+                sredniy_pixel[0] += kartinka[x+x_][y+y_][0]
+                sredniy_pixel[1] += kartinka[x+x_][y+y_][1]
+                sredniy_pixel[2] += kartinka[x+x_][y+y_][2]
+    return sredniy_pixel, pixels_in_average
---
+++ trunk/ii200113/task_01/main.py
@@ -0,0 +1,65 @@
+import cv2 
+import functions 
+from PyQt5 import uic 
+from PyQt5.QtWidgets import (
+    QLabel, QSlider, QSpinBox, QPushButton, QDialog, QFileDialog, QApplication
+)
+import sys
+
+class GlavnoeOkno(QDialog): 
+    def __init__(self):    
+        super(GlavnoeOkno, self).__init__()
+        uic.loadUi("ui/GlavnoeOkno.ui", self)
+        self.setWindowTitle("Пороговый филтр, лаба 1")
+        self.show()
+
+        self.image = cv2.imread("image/kartinka.jpg")
+        self.textshum = self.findChild(QLabel, "shum_text")
+        self.textFilter = self.findChild(QLabel, "threshold_text")
+        self.slidershum = self.findChild(QSlider, "shum_slider")
+        self.sliderChangeFilter = self.findChild(QSlider, "filter_slider")
+        self.spinBox1 = self.findChild(QSpinBox, "spinbox1")
+        self.spinBox2 = self.findChild(QSpinBox, "spinbox2")
+        self.buttonLoad = self.findChild(QPushButton, "load_button")
+        self.buttonshum = self.findChild(QPushButton, "shum_button")
+        self.buttonFilter = self.findChild(QPushButton, "filter_button")
+        self.knopkaKartinki = self.findChild(QPushButton, "show_button")
+        self.buttonLoad.clicked.connect(self.zagruzkaKartinki)
+        self.buttonshum.clicked.connect(self.applyshum)
+        self.buttonFilter.clicked.connect(self.otfiltrovatKartinku)
+        self.knopkaKartinki.clicked.connect(self.showImage)
+        self.slidershum.valueChanged.connect(self.slidershumTick)
+        self.sliderChangeFilter.valueChanged.connect(self.sliderChangeFilterTick)
+
+    def slidershumTick(self): 
+        self.textshum.setText("shum: "+str(self.slidershum.value()) + "%")
+
+    def sliderChangeFilterTick(self):
+        self.textFilter.setText("porog: "+str(self.sliderChangeFilter.value()))
+
+    def zagruzkaKartinki(self): 
+        path = QFileDialog.getOpenFileName()
+        self.image = cv2.imread(path[0])
+
+        self.showImage()
+
+
+    def applyshum(self): 
+        self.image = functions.shum(self.image, self.slidershum.value())
+        self.showImage()
+
+    def otfiltrovatKartinku(self):
+        self.image = functions.deshum(self.image, self.sliderChangeFilter.value(), self.spinBox1.value(), self.spinBox2.value())
+        self.showImage()
+
+    def showImage(self, wndout = False): 
+        if (wndout):
+            functions.pokaz_photo(self.image, "результаты")
+        else:
+            functions.pokaz_photo(self.image, "главное окно")
+
+
+if __name__ == '__main__':
+    application = QApplication(sys.argv)
+    zapusk = GlavnoeOkno()
+    application.exec_()
