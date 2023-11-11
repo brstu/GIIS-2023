0c1a9a169095a3c0ff1fe246b74f00d5bef048a0
Nikita <tacetprophetam@mail.ru>
2023-11-11 12:21:09 +0300

    task_01_da

--- trunk/ii200113/task_01/functions.py
+++ trunk/ii200113/task_01/functions.py
@@ -13,7 +13,8 @@ def shum(kartinka):
             print(str(int(pokazatel))+"%")
         buffer = pokazatel
         for y, _ in enumerate(stroka):
-            zarandomlennoe_v = random.randrange(1,5) 
+            zamanuha = range(1, 5)
+            zarandomlennoe_v = random.randint(zamanuha) 
             kartinka[x][y] = [zarandomlennoe_v*50,zarandomlennoe_v*50,zarandomlennoe_v*50]
     print('100%')
     return kartinka
