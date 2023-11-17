b07231729d842b5629dbc40db0ad88cf0062df69
akayohonda <evgesha-2@bk.ru>
2023-11-17 22:04:44 +0300

    task_04_fix

--- trunk/ii200095/task_03/src/script.js
+++ trunk/ii200095/task_03/src/script.js
@@ -1,14 +1,6 @@
-let menu = document.querySelector('.burger_menu');
-
-
-
-
-
-const sliderContainer = document.querySelector('.slider-container');
-const slider = document.querySelector('.slider');
-const slides = Array.from(document.querySelectorAll('.slide'));
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
+const slides = Array.from(document.querySelectorAll(".slide"));
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
 
 let slideWidth = slides[0].offsetWidth;
 let currentIndex = 0;
@@ -67,158 +59,13 @@ function initSlider() {
 }
 
 // Обработка изменения размера экрана
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   checkScreenSize();
   updateSlideWidth();
 });
 
 // Запуск инициализации слайдера при загрузке страницы
 initSlider();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function openburger(){
-    let menu = document.querySelector('.burger_menu');
-    document.querySelector('.wrapper').style.display='none';
-
-
-    document.querySelector('.moroj').style.display='none';
-    menu.style.display = 'block';
-}
-
-function closeburger(){
-    let menu = document.querySelector('.burger_menu');
-    document.querySelector('.wrapper').style.display='block';
-
-
-    document.querySelector('.moroj').style.display='block';
-    menu.style.display = 'none';
-}
-
-
-
-
-
-function openbuymenu320(){
-    let menu = document.querySelector('.burger_menu');
-    document.querySelector('.wrapper').style.display='block';
-    menu.style.display = 'none';
-    document.querySelector('.buy').style.display='block';
-    document.querySelector('.first_after_header_section').style.display='none';
-    document.querySelector('.second_section').style.display='none';
-    document.querySelector('.third_section').style.display='none';
-    document.querySelector('.fourth_section').style.display='none';
-    document.querySelector('.fifth_section').style.display='none';
-    document.querySelector('.button_section').style.display='none';
-
-    document.querySelector('.tochki').style.display='none';
-    document.querySelector('.tarelkaMoroj').style.display='none';
-    document.querySelector('.moroj').style.display='none';
-    
-    document.querySelector('.lorem_text').style.display='none'
-    document.querySelector('.wrapper967x664').style.height = 'auto'
-}
-
-function openbuymenu(){
-    document.querySelector('.wrapper967x664').style.height = 'auto'
-    document.querySelector('.buy').style.display='block';
-    document.querySelector('.first_after_header_section').style.display='none';
-    document.querySelector('.second_section').style.display='none';
-    document.querySelector('.third_section').style.display='none';
-    document.querySelector('.fourth_section').style.display='none';
-    document.querySelector('.fifth_section').style.display='none';
-    document.querySelector('.button_section').style.display='none';
-    document.querySelector('.tochki').style.display='none';
-    document.querySelector('.tarelkaMoroj').style.display='none';
-    document.querySelector('.moroj').style.display='none';
-    document.querySelector('.lorem_text').style.display='none'
-}
-
-function gobacktohome(){
-    document.querySelector('.buy').style.display='none';
-    const screenWidth = window.screen.width; 
-    console.log('screenWidth')
-    console.log(screenWidth)
-    document.querySelector('.first_after_header_section').style.display='flex';
-    document.querySelector('.second_section').style.display='block';
-    document.querySelector('.third_section').style.display='block';
-    document.querySelector('.fourth_section').style.display='block';
-    document.querySelector('.fifth_section').style.display='block';
-    document.querySelector('.moroj').style.display='block';
-    document.querySelector('.button_section').style.display='flex';
-    if(screenWidth>=768){
-        document.querySelector('.tarelkaMoroj').style.display='block';
-        document.querySelector('.lorem_text').style.display='block'
-        if(screenWidth==768){
-            document.querySelector('.wrapper967x664').style.height = '415px'
-        }
-        if(screenWidth>=1024){
-            document.querySelector('.tochki').style.display='block';
-            document.querySelector('.wrapper967x664').style.height = '664px'
-            document.querySelector('.tochki').style.display='block';
-        }
-    }
-}
-
-function gobacktohome320(){
-    document.querySelector('.buy').style.display='none';
-    let menu = document.querySelector('.burger_menu');
-    document.querySelector('.wrapper').style.display='block';
-    menu.style.display = 'none';
-    document.querySelector('.wrapper967x664').style.height = '502px'
-    document.querySelector('.first_after_header_section').style.display='flex';
-    document.querySelector('.second_section').style.display='block';
-    document.querySelector('.third_section').style.display='block';
-    document.querySelector('.fourth_section').style.display='block';
-    document.querySelector('.fifth_section').style.display='block';
-    document.querySelector('.moroj').style.display='block';
-    document.querySelector('.button_section').style.display='flex';
-    document.querySelector('.moroj').style.display='block';
-}
-
-
-function milkshakes(){
-    document.getElementById('1').src = './jpg/milkshake1.jpg'
-    document.getElementById('2').src = './jpg/milkshake2.jpeg'
-    document.getElementById('3').src = './jpg/milkshake3.jpg'
-    document.getElementById('4').src = './jpg/milkshale4.jpg'
-    document.getElementById('5').src = './jpg/milkshake5.jpg'
-    document.getElementById('6').src = './jpg/milkshake6.jpg'
-}
-
-function icecream(){
-    document.getElementById('1').src = './png/brooke-lark-8beTH4VkhLI-unsplash 1.png'
-    document.getElementById('2').src = './png/courtney-cook-QYsRxRPygwU-unsplash 1.png'
-    document.getElementById('3').src = './png/kenta-kikuchi-LZ6BTZnizD8-unsplash 1.png'
-    document.getElementById('4').src = './png/image 2.png'
-    document.getElementById('5').src = './png/image 3.png'
-    document.getElementById('6').src = './png/image 4.png'
-}
-
-function icecofee(){
-    document.getElementById('1').src = './jpg/icecoffee1.jpeg'
-    document.getElementById('2').src = './jpg/icecoffe2.jpg'
-    document.getElementById('3').src = './jpg/icecoffee3.jpg'
-    document.getElementById('4').src = './jpg/icecoffee4.jpg'
-    document.getElementById('5').src = './jpg/icecoffee5.jpg'
-    document.getElementById('6').src = './jpg/icecoffee6.jpg'
-}
 
 const prices = [3,5,7]
 let price = prices[0]
@@ -243,20 +90,3 @@ document.querySelector('.choise3').addEventListener('click',()=>{
   document.querySelector('.choise3').style.background = '#D6936D'
   document.querySelector('.choise1').style.background = '#fff'
 })
-
-
-function calculate(){
-
-  let quantity = document.querySelector('.Quantity').value
-  if(quantity){
-    let value = String(price * quantity)
-    document.querySelector('.buy_btn_numb').innerHTML = value 
-  }
-  else{
-    alert('Введите количество товара')
-  }
-  
-}
-
-
-
