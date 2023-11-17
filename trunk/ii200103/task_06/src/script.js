209f424c8861ed6b25cd86986c8283fc9fe27735
Evgeniy Doroshuk <e.doroshuk@estentech.com>
2023-11-17 20:06:04 +0300

    init

---
+++ trunk/ii200103/task_06/src/script.js
@@ -0,0 +1,18 @@
+let isDay = true
+
+// Изменение фона каждые 3 секунды
+function toggleWeather() {
+  const body = document.querySelector('body')
+
+  if (isDay) {
+    body.classList.add('night')
+    isDay = false
+  } else {
+    body.classList.remove('night')
+    isDay = true
+  }
+}
+
+setInterval(() => {
+  toggleWeather()
+}, 3000)
