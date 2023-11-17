84b5c1396644e85b4f70859daa25a735babaedc5
Evgeniy Doroshuk <e.doroshuk@estentech.com>
2023-11-17 20:19:57 +0300

    add .prettierrc.js

--- trunk/ii200103/task_06/src/script.js
+++ trunk/ii200103/task_06/src/script.js
@@ -1,18 +1,18 @@
-let isDay = true
+let isDay = true;
 
 // Изменение фона каждые 3 секунды
 function toggleWeather() {
-  const body = document.querySelector('body')
+  const body = document.querySelector('body');
 
   if (isDay) {
-    body.classList.add('night')
-    isDay = false
+    body.classList.add('night');
+    isDay = false;
   } else {
-    body.classList.remove('night')
-    isDay = true
+    body.classList.remove('night');
+    isDay = true;
   }
 }
 
 setInterval(() => {
-  toggleWeather()
-}, 3000)
+  toggleWeather();
+}, 3000);
