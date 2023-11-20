c852768fbf3a7c2fb778912b0e8b337be8df0f39
El_Xoce <teror714uns@gmail.com>
2023-11-20 18:08:17 +0300

    fix: code_duplicates

--- trunk/ii200111/task_04_05/src/scripts.js
+++ trunk/ii200111/task_04_05/src/scripts.js
@@ -2,63 +2,22 @@ let BJgame = {
     'you': {'scoreSpan': '#yourscore', 'div': '#your-box', 'score': 0},
     'dealer': {'scoreSpan': '#dealerscore', 'div': '#dealer-box', 'score': 0},
 
-    'cards': ['2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'KC', 'QC', 'JC', 'AC', '2D', '3D', '4D', '5D',
-        '6D', '7D', '8D', '9D', '10D', 'KD', 'QD', 'JD', 'AD', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H',
-        'KH', 'QH', 'JH', 'AH', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'KS', 'QS', 'JS', 'AS'],
+    'cards': [],
+    'cardsmap': {},
 
-    'cardsmap': {
-        '2C': 2,
-        '3C': 3,
-        '4C': 4,
-        '5C': 5,
-        '6C': 6,
-        '7C': 7,
-        '8C': 8,
-        '9C': 9,
-        '10C': 10,
-        'KC': 10,
-        'QC': 10,
-        'JC': 10,
-        'AC': [1, 11],
-        '2D': 2,
-        '3D': 3,
-        '4D': 4,
-        '5D': 5,
-        '6D': 6,
-        '7D': 7,
-        '8D': 8,
-        '9D': 9,
-        '10D': 10,
-        'KD': 10,
-        'QD': 10,
-        'JD': 10,
-        'AD': [1, 11],
-        '2H': 2,
-        '3H': 3,
-        '4H': 4,
-        '5H': 5,
-        '6H': 6,
-        '7H': 7,
-        '8H': 8,
-        '9H': 9,
-        '10H': 10,
-        'KH': 10,
-        'QH': 10,
-        'JH': 10,
-        'AH': [1, 11],
-        '2S': 2,
-        '3S': 3,
-        '4S': 4,
-        '5S': 5,
-        '6S': 6,
-        '7S': 7,
-        '8S': 8,
-        '9S': 9,
-        '10S': 10,
-        'KS': 10,
-        'QS': 10,
-        'JS': 10,
-        'AS': [1, 11]
+    'initializeCards': function() {
+        const suits = ['C', 'D', 'H', 'S'];
+        const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'Q', 'J', 'A'];
+
+        for (const suit of suits) {
+            for (const value of cardValues) {
+                const card = value + suit;
+                const cardValue = (value === 'A') ? [1, 11] : (value === 'K' || value === 'Q' || value === 'J') ? 10 : parseInt(value);
+
+                this.cards.push(card);
+                this.cardsmap[card] = cardValue;
+            }
+        }
     },
 
     'wins': 0,
@@ -66,6 +25,11 @@ let BJgame = {
     'draws': 0,
     'tokens': 20
 };
+
+// Initialize cards and cardsmap
+BJgame.initializeCards();
+
+
 const You = BJgame['you'];
 const Dealer = BJgame['dealer'];
 
