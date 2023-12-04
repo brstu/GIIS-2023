afe50cc8ad6bd4f7b1d64eeb6f858720b9091bbe
slavenia <slavakovalets@gmail.com>
2023-12-04 21:47:14 +0300

    task_03_fix3

--- trunk/ii200106/task_03/main.js
+++ trunk/ii200106/task_03/main.js
@@ -1,6 +1,2 @@
 const navbar = document.querySelector("#navbart");
 const div = document.querySelector(".none");
-
-navbar.addEventListener("click", () => {
-    div.classList.toggle("active");
-});
