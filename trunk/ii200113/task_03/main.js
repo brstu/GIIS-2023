ed78fdb78f081ee72f2ef856b3952286bc7808fb
Nikita <tacetprophetam@mail.ru>
2023-12-07 03:38:30 +0300

    task_04-05_fix3

--- trunk/ii200113/task_03/main.js
+++ trunk/ii200113/task_03/main.js
@@ -1,6 +1,1 @@
-const navbar = document.querySelector("#navbart");
-const div = document.querySelector(".none");
-
-navbar.addEventListener("click", () => {
-    div.classList.toggle("active");
-});
+console.log("Welcome!");
