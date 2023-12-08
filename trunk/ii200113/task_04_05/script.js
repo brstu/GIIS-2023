876b96a99db0345832ec34b2a269ee0ba7a98f57
Nikita <tacetprophetam@mail.ru>
2023-12-09 00:24:55 +0300

    task_04_05_fix17

--- trunk/ii200113/task_04_05/script.js
+++ trunk/ii200113/task_04_05/script.js
@@ -1,65 +1,68 @@
-import Grid from "./Grid.js"
-import Tile from "./Tile.js"
+import Grid from "./Grid.js";
+import Tile from "./Tile.js";
 
-const gameBoard = document.getElementById("game-board")
+const gameBoard = document.getElementById("game-board");
 
-const grid = new Grid(gameBoard)
-grid.randomEmptyCell().tile = new Tile(gameBoard)
-grid.randomEmptyCell().tile = new Tile(gameBoard)
-setupInput()
+const grid = new Grid(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+setupInput();
 
 function setupInput() {
-  window.addEventListener("keydown", handleInput, { once: true })
+  window.addEventListener("keydown", handleInput, { once: true });
 }
 
 async function handleInput(e) {
-  switch (e.key) {
-    case "ArrowUp":
-      if (!canMoveUp()) {
-        setupInput()
-        return
-      }
-      await moveUp()
-      break
-    case "ArrowDown":
-      if (!canMoveDown()) {
-        setupInput()
-        return
-      }
-      await moveDown()
-      break
-    case "ArrowLeft":
-      if (!canMoveLeft()) {
-        setupInput()
-        return
-      }
-      await moveLeft()
-      break
-    case "ArrowRight":
-      if (!canMoveRight()) {
-        setupInput()
-        return
-      }
-      await moveRight()
-      break
-    default:
-      setupInput()
-      return
+  try {
+    switch (e.key) {
+      case "ArrowUp":
+        if (!canMoveUp()) {
+          setupInput();
+          return;
+        }
+        await moveUp();
+        break;
+      case "ArrowDown":
+        if (!canMoveDown()) {
+          setupInput();
+          return;
+        }
+        await moveDown();
+        break;
+      case "ArrowLeft":
+        if (!canMoveLeft()) {
+          setupInput();
+          return;
+        }
+        await moveLeft();
+        break;
+      case "ArrowRight":
+        if (!canMoveRight()) {
+          setupInput();
+          return;
+        }
+        await moveRight();
+        break;
+      default:
+        setupInput();
+        return;
+    }
+
+    grid.cells.forEach((cell) => cell.mergeTiles());
+
+    const newTile = new Tile(gameBoard);
+    grid.randomEmptyCell().tile = newTile;
+
+    if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+      await newTile.waitForTransition(true);
+      alert("You lose");
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    setupInput();
   }
-
-  grid.cells.forEach(cell => cell.mergeTiles())
-
-  const newTile = new Tile(gameBoard)
-  grid.randomEmptyCell().tile = newTile
-
-  if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
-    newTile.waitForTransition(true).then(() => {
-      alert("You lose")
-    })
-    return
-  }
-
-  setupInput()
 }
 
 function moveUp() {
