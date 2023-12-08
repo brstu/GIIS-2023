1d3f0e193cef54c96d87a09472739bdc4c5e410f
Nikita <tacetprophetam@mail.ru>
2023-12-08 22:01:01 +0300

    task_04_05_fix9

--- trunk/ii200113/task_04_05/app.js
+++ trunk/ii200113/task_04_05/app.js
@@ -1,51 +1,51 @@
-document.addEventListener('DOMContentLoaded', () =>  {
-  const gridDisplay = document.querySelector('.grid')
-  const scoreDisplay = document.getElementById('score')
-  const resultDisplay = document.getElementById('result')
-  let squares = []
-  const width = 4
-  let score = 0
-
-  const crypto = require('crypto');
-  function secureRandom() {
+document.addEventListener("DOMContentLoaded", () => {
+  const gridDisplay = document.querySelector(".grid")
+  const scoreDisplay = document.getElementById("score")
+  const resultDisplay = document.getElementById("result")
+  let squares = [];
+  const width = 4;
+  let score = 0;
+  const crypto = require("crypto");
+  function secureRandom(){
     return crypto.randomInt(256);
   }
-  function createBoard() {
-    for (let i=0; i < width*width; i++) {
-      let square = document.createElement('div')
-      square.innerHTML = 0
-      gridDisplay.appendChild(square)
-      squares.push(square)
+  function createBoard(){
+    for (let i=0; i < width * width; i++) {
+      let square = document.createElement("div");
+      square.innerHTML = 0;
+      gridDisplay.appendChild(square);
+      squares.push(square);
     }
-    generate()
-    generate()
+    generate();
+    generate();
   }
-  
-  createBoard()
-  function generate() {
+  createBoard();
+  function generate(){
     let randomNumber = secureRandom();
     if (squares[randomNumber].innerHTML == 0) {
-      squares[randomNumber].innerHTML = 2
-      checkForGameOver()
-    } else generate()
+      squares[randomNumber].innerHTML = 2;
+      checkForGameOver();
+    } else { 
+      generate();
+    }
   }
 
-  function moveRight() {
+  function moveRight(){
     for (let i=0; i < 16; i++) {
       if (i % 4 === 0) {
-        let totalOne = squares[i].innerHTML
-        let totalTwo = squares[i+1].innerHTML
-        let totalThree = squares[i+2].innerHTML
-        let totalFour = squares[i+3].innerHTML
-        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-        let filteredRow = row.filter(num => num)
-        let missing = 4 - filteredRow.length
-        let zeros = Array(missing).fill(0)
-        let newRow = zeros.concat(filteredRow)
-        squares[i].innerHTML = newRow[0]
-        squares[i +1].innerHTML = newRow[1]
-        squares[i +2].innerHTML = newRow[2]
-        squares[i +3].innerHTML = newRow[3]
+        let totalOne = squares[i].innerHTML;
+        let totalTwo = squares[i+1].innerHTML;
+        let totalThree = squares[i+2].innerHTML;
+        let totalFour = squares[i+3].innerHTML;
+        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)];
+        let filteredRow = row.filter(num => num);
+        let missing = 4 - filteredRow.length;
+        let zeros = Array(missing).fill(0);
+        let newRow = zeros.concat(filteredRow);
+        squares[i].innerHTML = newRow[0];
+        squares[i +1].innerHTML = newRow[1];
+        squares[i +2].innerHTML = newRow[2];
+        squares[i +3].innerHTML = newRow[3];
       }
     }
   }
@@ -53,19 +53,19 @@ document.addEventListener('DOMContentLoaded', () =>  {
   function moveLeft() {
     for (let i=0; i < 16; i++) {
       if (i % 4 === 0) {
-        let totalOne = squares[i].innerHTML
-        let totalTwo = squares[i+1].innerHTML
-        let totalThree = squares[i+2].innerHTML
-        let totalFour = squares[i+3].innerHTML
-        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-        let filteredRow = row.filter(num => num)
-        let missing = 4 - filteredRow.length
-        let zeros = Array(missing).fill(0)
-        let newRow = filteredRow.concat(zeros)
-        squares[i].innerHTML = newRow[0]
-        squares[i +1].innerHTML = newRow[1]
-        squares[i +2].innerHTML = newRow[2]
-        squares[i +3].innerHTML = newRow[3]
+        let totalOne = squares[i].innerHTML;
+        let totalTwo = squares[i+1].innerHTML;
+        let totalThree = squares[i+2].innerHTML;
+        let totalFour = squares[i+3].innerHTML;
+        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)];
+        let filteredRow = row.filter(num => num);
+        let missing = 4 - filteredRow.length;
+        let zeros = Array(missing).fill(0);
+        let newRow = filteredRow.concat(zeros);
+        squares[i].innerHTML = newRow[0];
+        squares[i +1].innerHTML = newRow[1];
+        squares[i +2].innerHTML = newRow[2];
+        squares[i +3].innerHTML = newRow[3];
       }
     }
   }
@@ -73,151 +73,151 @@ document.addEventListener('DOMContentLoaded', () =>  {
 
   function moveUp() {
     for (let i=0; i < 4; i++) {
-      let totalOne = squares[i].innerHTML
-      let totalTwo = squares[i+width].innerHTML
-      let totalThree = squares[i+(width*2)].innerHTML
-      let totalFour = squares[i+(width*3)].innerHTML
-      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-      let filteredColumn = column.filter(num => num)
-      let missing = 4 - filteredColumn.length
-      let zeros = Array(missing).fill(0)
-      let newColumn = filteredColumn.concat(zeros)
-      squares[i].innerHTML = newColumn[0]
-      squares[i +width].innerHTML = newColumn[1]
-      squares[i+(width*2)].innerHTML = newColumn[2]
-      squares[i+(width*3)].innerHTML = newColumn[3]
+      let totalOne = squares[i].innerHTML;
+      let totalTwo = squares[i+width].innerHTML;
+      let totalThree = squares[i+(width*2)].innerHTML;
+      let totalFour = squares[i+(width*3)].innerHTML;
+      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)];
+      let filteredColumn = column.filter(num => num);
+      let missing = 4 - filteredColumn.length;
+      let zeros = Array(missing).fill(0);
+      let newColumn = filteredColumn.concat(zeros);
+      squares[i].innerHTML = newColumn[0];
+      squares[i +width].innerHTML = newColumn[1];
+      squares[i+(width*2)].innerHTML = newColumn[2];
+      squares[i+(width*3)].innerHTML = newColumn[3];
     }
   }
 
   function moveDown() {
     for (let i=0; i < 4; i++) {
-      let totalOne = squares[i].innerHTML
-      let totalTwo = squares[i+width].innerHTML
-      let totalThree = squares[i+(width*2)].innerHTML
-      let totalFour = squares[i+(width*3)].innerHTML
-      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-      let filteredColumn = column.filter(num => num)
-      let missing = 4 - filteredColumn.length
-      let zeros = Array(missing).fill(0)
-      let newColumn = zeros.concat(filteredColumn)
-      squares[i].innerHTML = newColumn[0]
-      squares[i +width].innerHTML = newColumn[1]
-      squares[i+(width*2)].innerHTML = newColumn[2]
-      squares[i+(width*3)].innerHTML = newColumn[3]
+      let totalOne = squares[i].innerHTML;
+      let totalTwo = squares[i+width].innerHTML;
+      let totalThree = squares[i+(width*2)].innerHTML;
+      let totalFour = squares[i+(width*3)].innerHTML;
+      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)];
+      let filteredColumn = column.filter(num => num);
+      let missing = 4 - filteredColumn.length;
+      let zeros = Array(missing).fill(0);
+      let newColumn = zeros.concat(filteredColumn);
+      squares[i].innerHTML = newColumn[0];
+      squares[i +width].innerHTML = newColumn[1];
+      squares[i+(width*2)].innerHTML = newColumn[2];
+      squares[i+(width*3)].innerHTML = newColumn[3];
     }
   }
 
   function combineRow() {
     for (let i =0; i < 15; i++) {
       if (squares[i].innerHTML === squares[i +1].innerHTML) {
-        let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +1].innerHTML)
-        squares[i].innerHTML = combinedTotal
-        squares[i +1].innerHTML = 0
-        score += combinedTotal
-        scoreDisplay.innerHTML = score
+        let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +1].innerHTML);
+        squares[i].innerHTML = combinedTotal;
+        squares[i +1].innerHTML = 0;
+        score += combinedTotal;
+        scoreDisplay.innerHTML = score;
       }
     }
-    checkForWin()
+    checkForWin();
   }
 
   function combineColumn() {
     for (const square of squares) {
       if (square.innerHTML === squares[i +width].innerHTML) {
-        let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +width].innerHTML)
-        squares[i].innerHTML = combinedTotal
-        squares[i +width].innerHTML = 0
-        score += combinedTotal
-        scoreDisplay.innerHTML = score
+        let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +width].innerHTML);
+        squares[i].innerHTML = combinedTotal;
+        squares[i +width].innerHTML = 0;
+        score += combinedTotal;
+        scoreDisplay.innerHTML = score;
       }
     }
-    checkForWin()
+    checkForWin();
   }
 
   function control(e) {
     if(e.keyCode === 37) {
-      keyLeft()
+      keyLeft();
     } else if (e.keyCode === 38) {
-      keyUp()
+      keyUp();
     } else if (e.keyCode === 39) {
-      keyRight()
+      keyRight();
     } else if (e.keyCode === 40) {
-      keyDown()
+      keyDown();
     }
   }
-  document.addEventListener('keyup', control)
+  document.addEventListener("keyup", control);
 
   function keyRight() {
-    moveRight()
-    combineRow()
-    moveRight()
-    generate()
+    moveRight();
+    combineRow();
+    moveRight();
+    generate();
   }
 
   function keyLeft() {
-    moveLeft()
-    combineRow()
-    moveLeft()
-    generate()
+    moveLeft();
+    combineRow();
+    moveLeft();
+    generate();
   }
 
   function keyUp() {
-    moveUp()
-    combineColumn()
-    moveUp()
-    generate()
+    moveUp();
+    combineColumn();
+    moveUp();
+    generate();
   }
 
   function keyDown() {
-    moveDown()
-    combineColumn()
-    moveDown()
-    generate()
+    moveDown();
+    combineColumn();
+    moveDown();
+    generate();
   }
 
   function checkForWin() {
     for (const square of squares) {
       if (square.innerHTML == 2048) {
-        resultDisplay.innerHTML = 'You WIN'
-        document.removeEventListener('keyup', control)
-        setTimeout(() => clear(), 3000)
+        resultDisplay.innerHTML = "You WIN";
+        document.removeEventListener("keyup", control);
+        setTimeout(() => clear(), 3000);
       }
     }
   }
 
   function checkForGameOver() {
-    let zeros = 0
+    let zeros = 0;
     for (const square of squares) {
       if (square.innerHTML == 0) {
-        zeros++
+        zeros++;
       }
     }
     if (zeros === 0) {
-      resultDisplay.innerHTML = 'You LOSE'
-      document.removeEventListener('keyup', control)
-      setTimeout(() => clear(), 3000)
+      resultDisplay.innerHTML = "You LOSE";
+      document.removeEventListener("keyup", control);
+      setTimeout(() => clear(), 3000);
     }
   }
 
   function clear() {
-    clearInterval(myTimer)
+    clearInterval(myTimer);
   }
 
   function addColours() {
     for (const square of squares) {
-      if (square.innerHTML == 0) squares[i].style.backgroundColor = '#afa192'
-      else if (squares[i].innerHTML == 2) squares[i].style.backgroundColor = '#eee4da'
-      else if (squares[i].innerHTML  == 4) squares[i].style.backgroundColor = '#ede0c8' 
-      else if (squares[i].innerHTML  == 8) squares[i].style.backgroundColor = '#f2b179' 
-      else if (squares[i].innerHTML  == 16) squares[i].style.backgroundColor = '#ffcea4' 
-      else if (squares[i].innerHTML  == 32) squares[i].style.backgroundColor = '#e8c064' 
-      else if (squares[i].innerHTML == 64) squares[i].style.backgroundColor = '#ffab6e' 
-      else if (squares[i].innerHTML == 128) squares[i].style.backgroundColor = '#fd9982' 
-      else if (squares[i].innerHTML == 256) squares[i].style.backgroundColor = '#ead79c' 
-      else if (squares[i].innerHTML == 512) squares[i].style.backgroundColor = '#76daff' 
-      else if (squares[i].innerHTML == 1024) squares[i].style.backgroundColor = '#beeaa5' 
-      else if (squares[i].innerHTML == 2048) squares[i].style.backgroundColor = '#d7d4f0' 
+      if (square.innerHTML == 0) squares[i].style.backgroundColor = "#afa192";
+      else if (squares[i].innerHTML == 2) squares[i].style.backgroundColor = "#eee4da";
+      else if (squares[i].innerHTML  == 4) squares[i].style.backgroundColor = "#ede0c8";
+      else if (squares[i].innerHTML  == 8) squares[i].style.backgroundColor = "#f2b179";
+      else if (squares[i].innerHTML  == 16) squares[i].style.backgroundColor = "#ffcea4";
+      else if (squares[i].innerHTML  == 32) squares[i].style.backgroundColor = "#e8c064";
+      else if (squares[i].innerHTML == 64) squares[i].style.backgroundColor = "#ffab6e";
+      else if (squares[i].innerHTML == 128) squares[i].style.backgroundColor = "#fd9982";
+      else if (squares[i].innerHTML == 256) squares[i].style.backgroundColor = "#ead79c";
+      else if (squares[i].innerHTML == 512) squares[i].style.backgroundColor = "#76daff";
+      else if (squares[i].innerHTML == 1024) squares[i].style.backgroundColor = "#beeaa5";
+      else if (squares[i].innerHTML == 2048) squares[i].style.backgroundColor = "#d7d4f0";
     }
 }
 addColours()
-var myTimer = setInterval(addColours, 50)
+let myTimer = setInterval(addColours, 50);
 })
