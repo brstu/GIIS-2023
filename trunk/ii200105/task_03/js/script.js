9cd571fddb71f1d3e69ba4925d4be0ff676d6887
Жук Александра <144815188+ZhukAlexandra@users.noreply.github.com>
2023-12-12 02:43:42 +0300

    fix

--- trunk/ii200105/task_03/js/script.js
+++ trunk/ii200105/task_03/js/script.js
@@ -1,108 +1,110 @@
 (function($) {
-    "use strict";
+  "use strict";
 
-    let initProductQty = function() {
-        $(".product-qty").each(function() {
-            let $el_product = $(this);
-            $el_product.find(".quantity-right-plus").click(function(e) {
-                e.preventDefault();
-                let quantity = parseInt($el_product.find("#quantity").val(), 10);
-                $el_product.find("#quantity").val(quantity + 1);
-            });
-            $el_product.find(".quantity-left-minus").click(function(e) {
-                e.preventDefault();
-                let quantity = parseInt($el_product.find("#quantity").val(), 10);
-                if (quantity > 0) {
-                    $el_product.find("#quantity").val(quantity - 1);
-                }
-            });
-        });
-    };
-
-    $(document).ready(function() {
-        let $videoSrc;
-        $(".play-btn").click(function() {
-            $videoSrc = $(this).data("src");
-        });
-
-        $("#myModal").on("shown.bs.modal", function() {
-            $("#video").attr("src", $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        });
-
-        $("#myModal").on("hide.bs.modal", function() {
-            $("#video").attr("src", $videoSrc);
-        });
-
-        new Swiper(".main-swiper", {
-            speed: 800,
-            loop: true,
-            pagination: {
-                el: ".main-slider-pagination",
-                clickable: true,
-            },
-        });
-
-        new Swiper(".product-swiper", {
-            speed: 800,
-            spaceBetween: 20,
-            navigation: {
-                nextEl: ".product-carousel-next",
-                prevEl: ".product-carousel-prev",
-            },
-            breakpoints: {
-                0: {
-                    slidesPerView: 1,
-                },
-                480: {
-                    slidesPerView: 2,
-                },
-                900: {
-                    slidesPerView: 3,
-                    spaceBetween: 20,
-                },
-                1200: {
-                    slidesPerView: 5,
-                    spaceBetween: 20,
-                },
-            },
-        });
-
-        new Swiper(".testimonial-swiper", {
-            speed: 800,
-            navigation: {
-                nextEl: ".testimonial-arrow-next",
-                prevEl: ".testimonial-arrow-prev",
-            },
-        });
-
-        new Swiper(".product-swiper2", {
-            speed: 800,
-            spaceBetween: 20,
-            navigation: {
-                nextEl: ".product-carousel-next2",
-                prevEl: ".product-carousel-prev2",
-            },
-            breakpoints: {
-                0: {
-                    slidesPerView: 1,
-                },
-                480: {
-                    slidesPerView: 2,
-                },
-                900: {
-                    slidesPerView: 3,
-                    spaceBetween: 20,
-                },
-                1200: {
-                    slidesPerView: 5,
-                    spaceBetween: 20,
-                },
-            },
-        });
-
-        new Swiper(".thumb-swiper", {
-            slidesPerView: 1,
-        });
-        initProductQty();
+  let initProductQty = function () {
+    $(".product-qty").each(function () {
+      let $el_product = $(this);
+      $el_product.find(".quantity-right-plus").click(function (e) {
+        e.preventDefault();
+        let quantity = parseInt($el_product.find("#quantity").val(), 10);
+        $el_product.find("#quantity").val(quantity + 1);
+      });
+      $el_product.find(".quantity-left-minus").click(function (e) {
+        e.preventDefault();
+        let quantity = parseInt($el_product.find("#quantity").val(), 10);
+        if (quantity > 0) {
+          $el_product.find("#quantity").val(quantity - 1);
+        }
+      });
     });
+  };
+
+  $(document).ready(function () {
+    let $videoSrc;
+    $(".play-btn").click(function () {
+      $videoSrc = $(this).data("src");
+    });
+    $("#myModal").on("shown.bs.modal", function () {
+      $("#video").attr(
+        "src",
+        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+      );
+    });
+    $("#myModal").on("hide.bs.modal", function () {
+      $("#video").attr("src", $videoSrc);
+    });
+
+    new Swiper(".main-swiper", {
+      speed: 800,
+      loop: true,
+      pagination: {
+        el: ".main-slider-pagination",
+        clickable: true,
+      },
+    });
+
+    new Swiper(".product-swiper", {
+      speed: 800,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: ".product-carousel-next",
+        prevEl: ".product-carousel-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        480: {
+          slidesPerView: 2,
+        },
+        900: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      },
+    });
+
+    new Swiper(".testimonial-swiper", {
+      speed: 800,
+      navigation: {
+        nextEl: ".testimonial-arrow-next",
+        prevEl: ".testimonial-arrow-prev",
+      },
+    });
+
+    new Swiper(".product-swiper2", {
+      speed: 800,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: ".product-carousel-next2",
+        prevEl: ".product-carousel-prev2",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        480: {
+          slidesPerView: 2,
+        },
+        900: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      },
+    });
+
+    new Swiper(".thumb-swiper", {
+      slidesPerView: 1,
+    });
+
+    initProductQty();
+  });
 })(jQuery);
